2cfdefe915f9dca041476b08387271f4d8e27552
ATHIRA PRASAD <133925955+athiragiri99@users.noreply.github.com>
2026-01-21 18:47:46 -0600

    Update stg_transactions.sql

--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -1,9 +1,8 @@
-sql
-select
-transaction_id,
-cutomer_id,
-amount,
-transaction_time,
-merchant,
-is_fraud
-from{{source('fraud_raw','transactions_raw')}}
+{{ config(materialized='view') }}
+SELECT Time, V1, V2, V3, V4, V5, V6, V7, V8, V9,
+       V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,
+       V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount,
+       TRY_TO_BOOLEAN(Class) as is_fraud
+FROM {{ source('raw', 'raw_transactions') }}
+WHERE Amount > 0
+
